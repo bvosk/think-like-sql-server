b441300e01c0f8eaa991a9b07f791403b533cdd3
Brian Voskerijian <brian.voskerijian@7factor.io>
2023-05-15 23:05:54 -0400

    Add index queries

---
+++ topics/indexes.sql
@@ -0,0 +1,24 @@
+SET STATISTICS IO ON;
+
+SELECT Id
+FROM dbo.Users
+WHERE LastAccessDate > '2014/07/01';
+
+CREATE INDEX
+IX_LastAccessDate_Id
+ON dbo.Users(LastAccessDate, Id)
+
+SELECT Id
+FROM dbo.Users
+WHERE LastAccessDate > '2014/07/01'
+ORDER BY LastAccessDate;
+
+SELECT Id
+FROM dbo.Users WITH (INDEX = 1)
+WHERE LastAccessDate > '2014/07/01'
+ORDER BY LastAccessDate;
+
+SELECT Id
+FROM dbo.Users
+WHERE LastAccessDate > '1800/07/01'
+ORDER BY LastAccessDate;

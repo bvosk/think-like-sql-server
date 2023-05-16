ec52f4cb8382284b1a17d3bc360a4e1bcbcceec0
Brian Voskerijian <brian.voskerijian@7factor.io>
2023-05-15 22:46:15 -0400

    Raise cost threshold for parallelism to simplify query plans

---
+++ scripts/configure_parallelism.sql
@@ -0,0 +1,11 @@
+-- Set the cost threshold for parallelism high to simplify query plans
+-- https://www.brentozar.com/archive/2019/10/why-query-plans-can-look-different-on-different-servers/
+-- https://learn.microsoft.com/en-us/sql/database-engine/configure-windows/configure-the-cost-threshold-for-parallelism-server-configuration-option?view=sql-server-ver16
+EXEC sp_configure 'show advanced options', 1;
+GO
+RECONFIGURE
+GO
+EXEC sp_configure 'cost threshold for parallelism', 1000;
+GO
+RECONFIGURE
+GO
--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 /opt/mssql-tools/bin/sqlcmd -U sa -S "$MSSQL_SERVER" -P "$MSSQL_SA_PASSWORD" -i /scripts/restore_db.sql
+/opt/mssql-tools/bin/sqlcmd -U sa -S "$MSSQL_SERVER" -P "$MSSQL_SA_PASSWORD" -i /scripts/configure_parallelism.sql
